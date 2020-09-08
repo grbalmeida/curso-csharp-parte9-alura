dc2416c0c166fbfdf790e0092104552fe8c9eb7c
gilvan <g.r.almeida@live.com>
2020-09-08 07:53:27 -0300

    Criando arquivos CSV

--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -8,47 +8,9 @@ namespace ByteBankImportacaoExportacao
     { 
         static void Main(string[] args) 
         {
-            var enderecoDoArquivo = "contas.txt";
-
-            using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
-            using (var leitor = new StreamReader(fluxoDeArquivo))
-            {
-                while (!leitor.EndOfStream)
-                {
-                    var linha = leitor.ReadLine();
-
-                    var contaCorrente = ConverterStringParaContaCorrente(linha);
-
-                    var msg = $"{contaCorrente.Titular.Nome} : Conta número {contaCorrente.Numero}, ag. {contaCorrente.Agencia}. Saldo: {contaCorrente.Saldo}";
-
-                    Console.WriteLine(msg);
-                }
-            }
+            CriarArquivo();
 
             Console.ReadLine();
-        }
-
-        static ContaCorrente ConverterStringParaContaCorrente(string linha)
-        {
-            string[] campos = linha.Split(',');
-
-            var agencia = campos[0];
-            var numero = campos[1];
-            var saldo = campos[2].Replace('.', ',');
-            var nomeTitular = campos[3];
-
-            var agenciaComoInt = int.Parse(agencia);
-            var numeroComoInt = int.Parse(numero);
-            var saldoComoDouble = double.Parse(saldo);
-
-            var titular = new Cliente();
-            titular.Nome = nomeTitular;
-
-            var resultado = new ContaCorrente(agenciaComoInt, numeroComoInt);
-            resultado.Depositar(saldoComoDouble);
-            resultado.Titular = titular;
-
-            return resultado;
         }
     }
 } 
