fa8a59320442f75738e53cb947a43d592debb69f
gilvan <g.r.almeida@live.com>
2020-09-08 20:50:40 -0300

    Usando o stream de entrada da Console

--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -8,7 +8,7 @@ namespace ByteBankImportacaoExportacao
     { 
         static void Main(string[] args) 
         {
-            LeituraBinaria();
+            UsarStreamDeEntrada();
 
             Console.ReadLine();
         }
