b2f98a1fdd17670aa6386db9197ec176c654749a
gilvan <g.r.almeida@live.com>
2020-09-07 21:20:51 -0300

    Método Close e using

--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -10,15 +10,16 @@ namespace ByteBankImportacaoExportacao
         {
             var enderecoDoArquivo = "contas.txt";
 
-            var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
-
-            var buffer = new byte[1024]; // 1 Kb
-            var numeroDeBytesLidos = -1;
-
-            while (numeroDeBytesLidos != 0)
+            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
             {
-                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-                EscreverBuffer(buffer);
+                var buffer = new byte[1024]; // 1 Kb
+                var numeroDeBytesLidos = -1;
+
+                while (numeroDeBytesLidos != 0)
+                {
+                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                    EscreverBuffer(buffer);
+                }
             }
 
             Console.ReadLine();
